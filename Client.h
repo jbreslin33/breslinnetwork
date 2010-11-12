4874193457189eb5f13548398087614028bb1b77
jbreslin33 <jbreslin33@localhost>
2010-11-11 21:56:00 -0500

    ..

---
+++ Client.h
@@ -0,0 +1,26 @@
+
+/*
+-----------------------------------------------------------------------------
+Filename:    GameServer.h
+*/
+#ifndef __CLIENT_h_
+#define __CLIENT_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+class Client
+{
+
+public:
+    Client();
+    virtual ~Client(void);
+
+
+protected:
+
+
+};
+
+#endif
